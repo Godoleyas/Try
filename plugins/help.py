6c4a9ea23e9775c3c5c4beeedd8bab9598e6489d
godoleyas <papygamer321@gmail.com>
2021-07-04 13:53:39 +0300

    huh

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Contact @PapyProjectsBot for help I will see it 😁"
+    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
     await message.reply_text(helptxt)
